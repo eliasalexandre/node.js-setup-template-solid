74f1dad7da5c770e3f29f6b4f2d15437df691da6
elias alexandre <elliasallex@gmail.com>
2021-10-04 23:15:06 -0300

    feat: vars to session

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "app-real-token",
+  expire_in_token: "15m",
+  secret_refresh_token: "app-real-refresh_token",
+  expire_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
