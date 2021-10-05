a1798e45a6ec7959f20cc1351d4c2d9ce00816d3
elias alexandre <elliasallex@gmail.com>
2021-10-04 23:07:14 -0300

    feat: routes def

---
+++ src/shared/infra/http/routes/users.routes.ts
@@ -0,0 +1,9 @@
+import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { Router } from "express";
+
+const usersRoutes = Router();
+
+const createUserController = new CreateUserController();
+usersRoutes.post("/", createUserController.handle);
+
+export { usersRoutes };
